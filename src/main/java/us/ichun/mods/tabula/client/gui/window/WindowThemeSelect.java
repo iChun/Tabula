21ca04480c66fb0b910f7b6321677477a3e26782
iChun <ohai.iChun@gmail.com>
2014-12-28 00:56:33 +0000

    Forgot about the other files.

---
+++ src/main/java/us/ichun/mods/tabula/client/gui/window/WindowThemeSelect.java
@@ -0,0 +1,97 @@
+package us.ichun.mods.tabula.client.gui.window;
+
+import com.google.common.io.ByteStreams;
+import com.google.gson.Gson;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import us.ichun.mods.tabula.client.core.ResourceHelper;
+import us.ichun.mods.tabula.client.export.ExportList;
+import us.ichun.mods.tabula.client.export.types.Exporter;
+import us.ichun.mods.tabula.client.gui.GuiWorkspace;
+import us.ichun.mods.tabula.client.gui.Theme;
+import us.ichun.mods.tabula.client.gui.window.element.Element;
+import us.ichun.mods.tabula.client.gui.window.element.ElementButton;
+import us.ichun.mods.tabula.client.gui.window.element.ElementListTree;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+public class WindowThemeSelect extends Window
+{
+    public ElementListTree modelList;
+    public Theme currentTheme;
+
+    public WindowThemeSelect(GuiWorkspace parent, int x, int y, int w, int h, int minW, int minH)
+    {
+        super(parent, x, y, w, h, minW, minH, "window.selectTheme.title", true);
+
+        currentTheme = Theme.copyInstance();
+
+        elements.add(new ElementButton(this, width - 140, height - 22, 60, 16, 1, false, 1, 1, "element.button.ok"));
+        elements.add(new ElementButton(this, width - 70, height - 22, 60, 16, 0, false, 1, 1, "element.button.cancel"));
+        modelList = new ElementListTree(this, BORDER_SIZE + 1, BORDER_SIZE + 1 + 10, width - (BORDER_SIZE * 2 + 2), height - BORDER_SIZE - 22 - 16, 3, false, false);
+        elements.add(modelList);
+
+        File def = new File(ResourceHelper.getThemesDir(), "default.json");
+
+        if(def.exists())
+        {
+            try
+            {
+                InputStream con = new FileInputStream(def);
+                String data = new String(ByteStreams.toByteArray(con));
+                con.close();
+
+                Theme theme = (new Gson()).fromJson(data, Theme.class);
+
+                modelList.createTree(null, theme, 13, 0, false, false);
+            }
+            catch(Exception ignored){}
+        }
+
+        File[] themes = ResourceHelper.getThemesDir().listFiles();
+
+        for(File file : themes)
+        {
+            if(!file.isDirectory() && FilenameUtils.getExtension(file.getName()).equals("json") && !file.getName().equalsIgnoreCase("default.json"))
+            {
+                try
+                {
+                    InputStream con = new FileInputStream(file);
+                    String data = new String(ByteStreams.toByteArray(con));
+                    con.close();
+
+                    Theme theme = (new Gson()).fromJson(data, Theme.class);
+
+                    modelList.createTree(null, theme, 13, 0, false, false);
+                }
+                catch(Exception ignored){}
+            }
+        }
+    }
+
+    @Override
+    public void draw(int mouseX, int mouseY)
+    {
+        super.draw(mouseX, mouseY);
+    }
+
+    @Override
+    public void elementTriggered(Element element)
+    {
+        if(element.id == 0)
+        {
+            Theme.loadTheme(currentTheme);
+            workspace.removeWindow(this, true);
+        }
+        if(element.id == 1 || element.id == 3)
+        {
+            if(workspace.windowDragged == this)
+            {
+                workspace.windowDragged = null;
+            }
+            workspace.removeWindow(this, true);
+        }
+    }
+}
---
+++ src/main/java/us/ichun/mods/tabula/client/gui/window/element/ElementCheckBox.java
@@ -0,0 +1,48 @@
+package us.ichun.mods.tabula.client.gui.window.element;
+
+import ichun.client.render.RendererHelper;
+import net.minecraft.util.StatCollector;
+import org.lwjgl.input.Mouse;
+import us.ichun.mods.tabula.client.gui.Theme;
+import us.ichun.mods.tabula.client.gui.window.Window;
+
+public class ElementCheckBox extends ElementToggle
+{
+    public ElementCheckBox(Window window, int x, int y, int w, int h, int ID, boolean igMin, int sideH, int sideV, String Tooltip, boolean state)
+    {
+        super(window, x, y, w, h, ID, igMin, sideH, sideV, "", Tooltip, state);
+    }
+
+
+    @Override
+    public void draw(int mouseX, int mouseY, boolean hover)
+    {
+        RendererHelper.drawColourOnScreen(Theme.instance.elementButtonBorder[0], Theme.instance.elementButtonBorder[1], Theme.instance.elementButtonBorder[2], 255, getPosX(), getPosY(), width, height, 0);
+        if(hover)
+        {
+            if(Mouse.isButtonDown(0))
+            {
+                RendererHelper.drawColourOnScreen(Theme.instance.elementButtonClick[0], Theme.instance.elementButtonClick[1], Theme.instance.elementButtonClick[2], 255, getPosX() + 1, getPosY() + 1, width - 2, height - 2, 0);
+            }
+            else
+            {
+                RendererHelper.drawColourOnScreen(Theme.instance.elementButtonBackgroundHover[0], Theme.instance.elementButtonBackgroundHover[1], Theme.instance.elementButtonBackgroundHover[2], 255, getPosX() + 1, getPosY() + 1, width - 2, height - 2, 0);
+            }
+        }
+        else
+        {
+            if(toggledState)
+            {
+                RendererHelper.drawColourOnScreen(Theme.instance.elementButtonToggle[0], Theme.instance.elementButtonToggle[1], Theme.instance.elementButtonToggle[2], 255, getPosX() + 1, getPosY() + 1, width - 2, height - 2, 0);
+            }
+            else
+            {
+                RendererHelper.drawColourOnScreen(Theme.instance.elementButtonBackgroundInactive[0], Theme.instance.elementButtonBackgroundInactive[1], Theme.instance.elementButtonBackgroundInactive[2], 255, getPosX() + 1, getPosY() + 1, width - 2, height - 2, 0);
+            }
+        }
+        if(toggledState)
+        {
+            parent.workspace.getFontRenderer().drawString("X", getPosX() + (width / 2) - (parent.workspace.getFontRenderer().getStringWidth(StatCollector.translateToLocal(text)) / 2), getPosY() + height - (height / 2) - (parent.workspace.getFontRenderer().FONT_HEIGHT / 2), Theme.getAsHex(!toggledState ? Theme.instance.elementButtonToggleHover : Theme.instance.font), false);
+        }
+    }
+}
